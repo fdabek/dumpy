3eab50589352784a32e0756f4390fe84738cd496
Frank Dabek <fdabek@gmail.com>
2016-10-26 23:14:51 -0400

    Recurse through directories

--- scan.go
+++ scan.go
@@ -9,44 +9,76 @@ import md5 "crypto/md5"
 import hex "encoding/hex"
 import "path"
 
-func main() {
-	filename := flag.String("directory", "", "Directory to scan")
-	flag.Parse()
+func walkDirectory(root string) <-chan string {
+	out := make(chan string)
+	var queue []string
+	queue = append(queue, root)
 
-	f,err := os.Open(*filename)
-	if err != nil {
-		log.Fatalf("Unable to open %s", *filename);
-	}
-
-	for {
-		files,err := f.Readdir(100)
-		if err == io.EOF {
-			log.Print("EOF on readdir")
-			break;
-		}
-		if err != nil {
-			log.Fatal("Readdir failed")
-		}
-		for _,stat := range files {
-			log.Printf("Name: %s\nSize: %d\n", stat.Name(), stat.Size())
-			path := path.Join(*filename, stat.Name())
-			f,err := os.Open(path)
+	go func() {
+		for len(queue) > 0 {
+			d := queue[0]
+			queue = queue[:1]
+		
+			f,err := os.Open(d)
 			if err != nil {
-				log.Printf("Couldn't open %s. Skipping it.", path)
+				log.Print("Unable to open: %s\n", d)
 				continue
 			}
+
 			for {
-				b := make([]byte, 1<<20)
-				n,err := f.Read(b)
-				if n == 0 {
-					log.Printf("EOF on %s", f.Name())
-					break
-				} else if err != nil {
-					log.Fatal("Non EOF error on ", f.Name())
+				infos,err := f.Readdir(100)
+				if err == io.EOF {
+					log.Print("EOF on readdir")
+					break;
 				}
-				csum := md5.Sum(b[:n])
-				fmt.Printf("%s: %s", f.Name(), hex.EncodeToString(csum[:]))
+				if err != nil {
+					log.Fatal("Readdir failed")
+				}
+
+				for _,stat := range infos {
+					full_path := path.Join(d, stat.Name())
+					if stat.IsDir() {
+						queue = append(queue, full_path)
+					} else {
+						out <- full_path
+					}
+				}
 			}
+			
+		}
+		close(out)
+	}()
+
+	return out
+}
+
+func hashFiles(files <-chan string) {
+	for path := range files {
+		f,err := os.Open(path)
+		if err != nil {
+			log.Printf("Couldn't open %s. Skipping it.", path)
+		}
+		for {
+			b := make([]byte, 1<<20)
+			n,err := f.Read(b)
+			if n == 0 {
+				log.Printf("EOF on %s", f.Name())
+				break
+			} else if err != nil {
+				log.Fatal("Non EOF error on ", f.Name())
+			}
+			csum := md5.Sum(b[:n])
+			fmt.Printf("%s: %s\n", f.Name(), hex.EncodeToString(csum[:]))
 		}
 	}
+}
+
+
+
+func main() {
+	root := flag.String("directory", "", "Directory to scan")
+	flag.Parse()
+
+	files := walkDirectory(*root)
+	hashFiles(files)
 }
